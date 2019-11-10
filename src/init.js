70a8002696cf5267904372092063b82cfccdb7cd
changjiaming [常佳明] <changjiaming@xiaomi.com>
2019-11-10 11:52:14 +0800

    .

--- src/init.js
+++ src/init.js
@@ -5,15 +5,21 @@ const fs = require('fs-extra')
 const { getFolderName } = require('./tools/tools')
 
 const repositories = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'repositories.json')))
+const branch = execSync('git branch').toString()
+console.log(branch)
 
 Object.keys(repositories).forEach(key => {
     const repo = repositories[key]
     const folderName = getFolderName(repo.url)
+    
+    if (branch.includes(key)) {
+        execSync(`git branch -D ${key}`)
+    }
     execSync(`git checkout -b ${key}`)
-    execSync(`git clone ${repo.url}`)
-    execSync(`cp -rf ./${folderName}/** ./`)
-    execSync('git add .')
-    execSync(`git commit -m "upload ${key}"`)
-    execSync(`git push origin ${key}`)
-    execSync(`git checkout master`)
+    execSync(`git clone ${repo.url}#${repo.version}`)
+    // execSync(`cp -rf ./${folderName}/** ./`)
+    // execSync('git add .')
+    // execSync(`git commit -m "upload ${key}"`)
+    // execSync(`git push origin ${key}`)
+    // execSync(`git checkout master`)
 })
